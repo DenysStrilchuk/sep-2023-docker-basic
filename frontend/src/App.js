600869be90ea6f553dc171b71f2f6564b4814f0e
Denys Sttrilchuk <denia876@gmail.com>
2024-04-09 19:41:58 +0300

    docker lesson

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [cars, setCars] = useState([])
 
-export default App;
+  useEffect(() => {
+    axios.get('http://owu.linkpc.net/carsAPI/v1/cars').then(({data})=> setCars(data));
+  }, []);
+    return (
+        <div>
+          {cars.map(car=><div key={car.id}>
+            {car.brand}
+          </div>)}
+        </div>
+    );
+};
+
+export {App};
